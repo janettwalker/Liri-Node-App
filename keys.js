f92b2b83bcfba0a31ec8b9b86825890458e2b0bd
Janet Walker <janetwalker271989@gmail.com>
2017-05-06 10:25:26 -0400

    Delete keys.js

--- keys.js
+++
@@ -1,9 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'RcRU1AT6UFjdTHEPDGU0tuUyH',
-  consumer_secret: 'dMytSpK2iKVRSTpiW5eFfPZV1DGZjHteXnhEziyCOMueZtIfG3',
-  access_token_key: '1877389592-jlB9dMaXGMo7yuRT1QdkcqTItBsdPJKgkWQRF1S',
-  access_token_secret: 'ujQpjYz1i09oM5tEncvYjyn8QrFn8pEATIQ5J4ZioXAJA',
-};
-
